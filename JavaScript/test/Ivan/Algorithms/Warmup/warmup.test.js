4055699068549e8ee8758e614e683b88454544e2
Ivan Neradovic <i.neradovic@levi9.com>
2017-12-12 09:24:18 +0100

    updating compare the triplets tests

--- JavaScript/test/Ivan/Algorithms/Warmup/warmup.test.js
+++ JavaScript/test/Ivan/Algorithms/Warmup/warmup.test.js
@@ -49,6 +49,33 @@ describe('Algorithms', function() {
             it('typeof [5, 6, 7, 8, 9, 10] array should return string', function() {
                 assert.equal('string', typeof myObj.compare(5, 6, 7, 8, 9, 10));
             });
+            it('x=1 y=2 compareTwo(3, 3) function, result should be x=1 y=2', function() {
+                myObj.x = 1;
+                myObj.y = 2;
+
+                myObj.compareTwo(3, 3);
+
+                assert.equal(1, myObj.x);
+                assert.equal(2, myObj.y);
+            });
+            it('x=1 y=2 compareTwo(3, 4) function, result should be x=1 y=3', function() {
+                myObj.x = 1;
+                myObj.y = 2;
+
+                myObj.compareTwo(3, 4);
+
+                assert.equal(1, myObj.x);
+                assert.equal(2+1, myObj.y);
+            });
+            it('x=1 y=2 compareTwo(4, 3) function, result should be x=2 y=2', function() {
+                myObj.x = 1;
+                myObj.y = 2;
+
+                myObj.compareTwo(4, 3);
+
+                assert.equal(1+1, myObj.x);
+                assert.equal(2, myObj.y);
+            });
             it('x=5 y=1 result() function should return \'5 1\'', function() {
                 myObj.x = 5;
                 myObj.y = 1;
